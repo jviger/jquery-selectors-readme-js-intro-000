cbee9ff9287b6f6fa868ff4cd781b953e3a39305
Jason Viger <jason.viger@gmail.com>
2018-07-12 11:56:48 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -3,7 +3,7 @@ function paragraphSelector() {
   return $('p');
 }
 function lastImageSelector() {
-  return $('img:last-child');
+  return $('img:last');
 }
 function ninjaBabySelector() {
   return $('#baby-ninja');
